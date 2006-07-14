70d1861c7f736572802b70617a849a8b069bc442
Joseph M. Ferner <joe@fernsroth.com>
2006-07-14 16:33:54 +0000

    use file name

--- src/main/java/com/fernsroth/squashfs/FileSystemDataProvider.java
+++ src/main/java/com/fernsroth/squashfs/FileSystemDataProvider.java
@@ -11,6 +11,7 @@ import com.fernsroth.easyio.EasyIORandomAccessFile;
 import com.fernsroth.easyio.IRandomAccessSource;
 import com.fernsroth.squashfs.model.BaseFile;
 import com.fernsroth.squashfs.model.Manifest;
+import com.fernsroth.squashfs.model.SFSSourceFile;
 
 /**
  * 
@@ -18,53 +19,58 @@ import com.fernsroth.squashfs.model.Manifest;
  */
 public class FileSystemDataProvider implements DataProvider {
 
-	/**
-	 * the source directory.
-	 */
-	private File sourceDir;
+    /**
+     * the source directory.
+     */
+    private File sourceDir;
 
-	/**
-	 * ino counter.
-	 */
-	private static int ino = 1;
+    /**
+     * ino counter.
+     */
+    private static int ino = 1;
 
-	/**
-	 * @param sourceDir
-	 */
-	public FileSystemDataProvider(File sourceDir) {
-		this.sourceDir = sourceDir;
-	}
+    /**
+     * @param sourceDir
+     */
+    public FileSystemDataProvider(File sourceDir) {
+        this.sourceDir = sourceDir;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public IRandomAccessSource getData(Manifest source, BaseFile bf)
-			throws IOException {
-		String path = source.getPath(bf);
-		File f = new File(this.sourceDir, path);
-		return new EasyIORandomAccessFile(f, "r");
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public IRandomAccessSource getData(Manifest source, BaseFile bf)
+            throws IOException {
+        if (bf instanceof SFSSourceFile) {
+            return new EasyIORandomAccessFile(((SFSSourceFile) bf)
+                    .getSourceFile(), "r");
+        } else {
+            String path = source.getPath(bf);
+            File f = new File(this.sourceDir, path);
+            return new EasyIORandomAccessFile(f, "r");
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public int getIno(Manifest source, BaseFile bf) {
-		return ino++;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public int getIno(Manifest source, BaseFile bf) {
+        return ino++;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public long getLength(Manifest source, BaseFile bf) throws IOException {
-		if (bf == null || bf.getName() == null) {
-			return 0;
-		}
-		String path = source.getPath(bf);
-		if (path == null) {
-			throw new FileNotFoundException("could not file '" + bf.getName()
-					+ "'");
-		}
-		File f = new File(this.sourceDir, path);
-		return f.length();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public long getLength(Manifest source, BaseFile bf) throws IOException {
+        if (bf == null || bf.getName() == null) {
+            return 0;
+        }
+        String path = source.getPath(bf);
+        if (path == null) {
+            throw new FileNotFoundException("could not file '" + bf.getName()
+                    + "'");
+        }
+        File f = new File(this.sourceDir, path);
+        return f.length();
+    }
 }
